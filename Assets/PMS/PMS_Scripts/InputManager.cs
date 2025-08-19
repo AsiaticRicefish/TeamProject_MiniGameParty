07e24d9489294948c2197e941937a85626801b3d
ParkMinSung2580 <114042812+ParkMinSung2580@users.noreply.github.com>
2025-08-20 00:41:49 +0900

    [Feat] 기초적인 슈팅 시스템
    마우스 클릭,클릭놓기를 통한 기초적인 슈팅 시스템

--- Assets/PMS/PMS_Scripts/InputManager.cs
+++ Assets/PMS/PMS_Scripts/InputManager.cs
@@ -5,43 +5,108 @@ public class InputManager : MonoBehaviour
 {
     [SerializeField] private Camera mainCam;
 
-    private UnimoEgg selectedMarble;
+    private PlayerInput playerInput; // PlayerInput 컴포넌트 참조 변수
+    private InputAction touchAction; // TouchPress 액션 참조 변수
+
+    private UnimoEgg selectedUnimoEgg;
+
+    private void Awake()
+    {
+        // PlayerInput 컴포넌트를 가져오고, 액션을 찾습니다.
+        playerInput = GetComponent<PlayerInput>();
+        if (playerInput != null)
+        {
+            touchAction = playerInput.actions.FindAction("TouchPress");
+        }
+    }
+    private void OnEnable()
+    {
+        // TouchPress 액션의 이벤트를 구독
+        if (touchAction != null)
+        {
+            touchAction.started += OnTouchPress;
+            touchAction.performed += OnTouchPress;
+            touchAction.canceled += OnTouchPress;
+            touchAction.Enable();
+        }
+    }
+
+    private void OnDisable()
+    {
+        // 스크립트가 비활성화될 때 이벤트 구독 해제
+        if (touchAction != null)
+        {
+            touchAction.started -= OnTouchPress;
+            touchAction.performed -= OnTouchPress;
+            touchAction.canceled -= OnTouchPress;
+            touchAction.Disable();
+        }
+    }
+
+    private void Update()
+    {
+        Debug.DrawRay(transform.position, transform.forward * 10f, Color.red); // 100f는 Ray의 길이, 1f는 표시 시간
+        //아니 레이가 왜 안보이지 기즈모를 껏나?  
+    }
 
     // TouchPress 이벤트 연결
     public void OnTouchPress(InputAction.CallbackContext ctx)
     {
-        Debug.Log("터치되고 있는지?");
-        if (Touchscreen.current == null) return;
+        Vector2 screenPos;
 
-        Vector3 touchPos = Touchscreen.current.primaryTouch.position.ReadValue();
-        Ray ray = mainCam.ScreenPointToRay(touchPos);
+        //왜 인지 모르겠는데 pc에서 자꾸 이상한데 값을 들고옴
+        //Vector2 touchPos = Touchscreen.current.primaryTouch.position.ReadValue();
 
-        var phase = Touchscreen.current.primaryTouch.phase.ReadValue();
-
-        if (phase == UnityEngine.InputSystem.TouchPhase.Began)
+        // 터치인지 마우스인지 확인해서 위치 가져오기
+        if (Touchscreen.current != null && Touchscreen.current.primaryTouch.press.isPressed)
         {
+            screenPos = Touchscreen.current.primaryTouch.position.ReadValue();
+        }
+        else if (Mouse.current != null)
+        {
+            screenPos = Mouse.current.position.ReadValue();
+        }
+        else
+        {
+            return;
+        }
+        Ray ray = mainCam.ScreenPointToRay(screenPos);
+        
+
+        Debug.DrawRay(ray.origin, ray.direction * 100f, Color.red); // 100f는 Ray의 길이, 1f는 표시 시간
+
+        if (ctx.started)
+        {
+            Debug.Log("start");
             // 터치 시작 → Raycast로 알 선택
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
-                selectedMarble = hit.collider.GetComponent<UnimoEgg>();
-                selectedMarble?.OnTouchStart(touchPos);
+                selectedUnimoEgg = hit.collider.GetComponent<UnimoEgg>();
+                Debug.Log(hit.collider.gameObject.name);
+                selectedUnimoEgg?.OnTouchStart(screenPos);          
             }
         }
-        else if (phase == UnityEngine.InputSystem.TouchPhase.Moved)
+        else if (ctx.performed)
         {
-            selectedMarble?.OnTouchMove(touchPos);
+            Debug.Log("move");
+            selectedUnimoEgg?.OnTouchMove(screenPos);
         }
-        else if (phase == UnityEngine.InputSystem.TouchPhase.Ended)
+        else if(ctx.canceled)
         {
-            selectedMarble?.OnTouchEnd(touchPos);
-            selectedMarble = null;
+            Debug.Log("end");
+            selectedUnimoEgg?.OnTouchEnd(screenPos);
+            selectedUnimoEgg = null;
+
+            if(selectedUnimoEgg == null)
+            {
+                Debug.Log("NULL처리완료");
+            }
         }
     }
 
-    // TouchPosition 이벤트는 필요 시 드래그 UI용으로 활용
     public void OnTouchPosition(InputAction.CallbackContext ctx)
     {
-        Vector3 pos = ctx.ReadValue<Vector3>();
+        Vector3 touchpos = ctx.ReadValue<Vector3>();
         // Debug.Log("터치 위치: " + pos);
     }
 }
--- Assets/PMS/PMS_Scripts/UnimoEgg.cs
+++ Assets/PMS/PMS_Scripts/UnimoEgg.cs
@@ -7,7 +7,7 @@ public class UnimoEgg : MonoBehaviour
     private Vector3 startTouchPos;
     private Vector3 endTouchPos;
 
-    [SerializeField] private float forceMultiplier = 5f;
+    private float forceMultiplier = 5f;
 
     private void Awake()
     {
@@ -17,16 +17,18 @@ public class UnimoEgg : MonoBehaviour
     //터치 시작했을 때
     public void OnTouchStart(Vector3 touchPos)
     {
-        startTouchPos = touchPos;
+        Debug.Log("유니모 찾아서 터치 시작함");
+        startTouchPos = touchPos;               //시작 위치를 저장
         rb.isKinematic = true; 
     }
 
     //터치 중일때
     public void OnTouchMove(Vector3 touchPos)
     {
-        // 드래그 중 시각적으로 이동하는 모습
-        Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3(touchPos.x, Camera.main.transform.position.y, touchPos.z));
-        transform.position = new Vector3(worldPos.x, transform.position.y, worldPos.z);
+        // 화면 좌표의 Y를 월드 좌표의 Z로 변환
+        //Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3(touchPos.x, touchPos.y, Camera.main.transform.position.y));
+        //transform.position = new Vector3(worldPos.x, transform.position.y, worldPos.z);
+        //생각해 보니깐 이동을 굳이 안해도된다.
     }
 
     //터치를 땟을 때
@@ -35,10 +37,20 @@ public class UnimoEgg : MonoBehaviour
         //TODO - 추후 나중에 놓은 시간만큼 forceMultiplier값을 높게 하여 곱해줘야한다.
         rb.isKinematic = false;
         endTouchPos = touchPos; // 직접 전달받은 좌표 사용(놓은 순간 해당 좌표)
-        Vector3 delta = startTouchPos - endTouchPos; //종료 -> 시작 방향 벡터
+
+        //항상 카메라는 x,y좌표 보다 Camera.main.transform.position.y 만큼 떨어져있다.
+        Vector3 startWorld = Camera.main.ScreenToWorldPoint(new Vector3(startTouchPos.x, startTouchPos.y, Camera.main.transform.position.y));
+        Vector3 endWorld = Camera.main.ScreenToWorldPoint(new Vector3(touchPos.x, touchPos.y, Camera.main.transform.position.y));
+        Vector3 dir = (startWorld - endWorld);
+        dir.y = 0;
+        rb.AddForce(dir.normalized * forceMultiplier, ForceMode.Impulse);
+
+        /*Vector3 dir = startTouchPos - endTouchPos; //종료 -> 시작 방향 벡터
+
+        dir = new Vector3(dir.x, 0 , dir.z);
         
         //Vector3 force = new Vector3(delta.x, 0, delta.y) * forceMultiplier;
 
-        rb.AddForce(delta.normalized * forceMultiplier, ForceMode.Impulse);
+        rb.AddForce(dir.normalized * forceMultiplier, ForceMode.Impulse);*/
     }
 }
