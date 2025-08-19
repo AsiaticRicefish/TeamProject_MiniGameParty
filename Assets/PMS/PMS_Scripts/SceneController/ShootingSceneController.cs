8cab8724ba8b7519cc296d78c3619ce2b66b9c1b
ParkMinSung2580 <114042812+ParkMinSung2580@users.noreply.github.com>
2025-08-19 10:39:15 +0900

    [Feat] ShootingGameScene 사전 작업
    BaseGameSceneController상속 받는 ShootingSceneController를 간단히 설계,
    게임 매니저,턴 매니저 스크립트 생성
    
    게임 매니저는 싱글톤으로 만들고  인터페이스로 IGameComponent, IGameStartHandler 를 받아 인터페이스 구현

---
+++ Assets/PMS/PMS_Scripts/SceneController/ShootingSceneController.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using ShootingScene_TurnManager;
+
+public class ShootingSceneController : BaseGameSceneController
+{
+    //따로 이벤트는 없는거 같음
+    public Action OnGameStarted;
+
+    protected override string GameType => "Shooting";
+
+    protected override IEnumerator WaitForManagersAwake()
+    {
+        yield return new WaitUntil(() =>
+            ShootingGameManager.Instance != null &&
+            TurnManager.Instance != null);
+
+        Debug.Log("모든 ShootingGameScene 매니저 Awake완료");
+    }
+
+    //순차 초기화
+    protected override IEnumerator InitializeSequentialManagers()
+    {
+        Debug.Log("ShootingGameScene 순차 초기화 시작");
+
+        var sequentialComponents = new List<IGameComponent>();
+
+        // 의존성 순서대로 추가
+        /* if (ShootingGameManager.Instance is IGameComponent netComp)
+            sequentialComponents.Add(netComp);*/
+
+        // 필요하다면 다른 순차 초기화 컴포넌트들 추가
+
+        yield return StartCoroutine(InitializeComponentsSafely(sequentialComponents));
+    }
+
+    //벙렬 초기화
+    protected override IEnumerator InitializeParallelManagers()
+    {
+        Debug.Log("ShootingGameScene 병렬 초기화 시작");
+
+        var parallelComponents = new List<ICoroutineGameComponent>();
+
+        // 독립적으로 초기화 가능한 컴포넌트들 추가
+        if (ShootingGameManager.Instance is ICoroutineGameComponent gameComp)
+            parallelComponents.Add(gameComp);
+
+        if (TurnManager.Instance is ICoroutineGameComponent uiComp)
+            parallelComponents.Add(uiComp);
+
+        yield return StartCoroutine(InitializeCoroutineComponentsSafely(parallelComponents));
+    }
+
+    
+    protected override void NotifyGameStart()
+    {
+        //OnGameStarted?.Invoke();
+
+        if (ShootingGameManager.Instance is IGameStartHandler gameStart)
+            gameStart.OnGameStart();
+
+        Debug.Log("Shooting 게임 시작 알림 완료");
+    }
+
+}
