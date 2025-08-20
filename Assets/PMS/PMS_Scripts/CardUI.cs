b468da4c4445e6b1cc8d9bbd744c25aab8bfb16e
ParkMinSung2580 <114042812+ParkMinSung2580@users.noreply.github.com>
2025-08-20 17:20:10 +0900

    [Feat] 카드 앞뒤 임시로 이미지 추가 및 Turn
    1.카드 앞뒤 임시로 이미지 추가 - 그림판사용
    
    2.CardFlip 스크립트 -> CardUI에 추가(하나의 스크립트로 관리)
    
    3.카드 뒤집히는데  faceSprite,backSprite로 변경되도록 설계 및 한번 클릭한 카드 다시 클릭 못하도록 이벤트 발생 함수에 if조건문 추가

--- Assets/PMS/PMS_Scripts/CardUI.cs
+++ Assets/PMS/PMS_Scripts/CardUI.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
@@ -8,11 +9,12 @@ namespace ShootingScene
 {
     public class CardUI : MonoBehaviour, IPointerClickHandler
     {
+        [SerializeField] 
+        private Sprite faceSprite,backSprite;
+
         [SerializeField] private Image cardBackGround;
         [SerializeField] private Image cardImage;
         [SerializeField] private Color ChangeColor;
-
-        [SerializeField] private CardFlip cardFlip;
 
         private bool isChecking;
 
@@ -21,20 +23,33 @@ namespace ShootingScene
 
         private Vector3 targetScale;
 
+        //CardFlip
+        private RectTransform rt;
+        private bool isFlipped = false;
+        private bool isFlipping = false;
+        private float flipDuration = 0.2f; // 뒤집는 시간
+        private Quaternion startRot;
+        private Quaternion endRot;
+
         public void Awake()
         {
-            cardFlip = GetComponent<CardFlip>();
+           rt = GetComponent<RectTransform>();
         }
         public void Start() => Subscribe();
 
         public void Subscribe()
         {
             //UI_Base.BindUIEvent(gameObject, (_) => OnClick());
+
+            //테스트 코드
+
             //UI가 손가락 터치를 인식했을 때 
-            //UI_Base.BindUIEvent(gameObject, (_) => OnClick(), LDH_Util.Define_LDH.UIEvent.PointEnter);
+            UI_Base.BindUIEvent(gameObject, (_) => OnClick(), LDH_Util.Define_LDH.UIEvent.PointEnter);
             //UI가 손가락 터치를 인식못했을 때
-            //UI_Base.BindUIEvent(gameObject, (_) => UnClick(), LDH_Util.Define_LDH.UIEvent.PointExit);
-            
+            UI_Base.BindUIEvent(gameObject, (_) => UnClick(), LDH_Util.Define_LDH.UIEvent.PointExit);
+
+            cardImage.sprite = backSprite;
+
         }
 
         public void OnClick()
@@ -51,12 +66,16 @@ namespace ShootingScene
         //추후 Dotween이나 Animation 사용하지 않을까
         public void sizeUp()
         {
+            if (isFlipped && !isFlipping) return;
+
             targetScale = hoverScale;
             transform.localScale = targetScale;
         }
 
         public void sizeDown()
         {
+            if (isFlipped && !isFlipping) return;
+
             targetScale = normalScale;
             transform.localScale = targetScale;
         }
@@ -64,13 +83,44 @@ namespace ShootingScene
         //IPointerClickHandler 필요
         public void OnPointerClick(PointerEventData eventData)
         {
-            cardFlip?.Flip();
+            if (isFlipped && !isFlipping) return;
+
+            Flip();
             Debug.Log("클릭함");
             //UI 180도 회전해서 다시 클릭을 못함
         }
 
         //IPointerDownHandler, IPointerUpHandler 필요
-        public void OnPointerDown(PointerEventData eventData) => OnClick();
-        public void OnPointerUp(PointerEventData eventData) => UnClick();
+        //public void OnPointerDown(PointerEventData eventData) => OnClick();
+        //public void OnPointerUp(PointerEventData eventData) => UnClick();
+
+
+        public void Flip()
+        {
+            if (isFlipping) return; // 진행 중이면 무시
+
+            isFlipped = !isFlipped;
+            startRot = rt.localRotation;                //시작 회전값을 저장
+            endRot = startRot * Quaternion.Euler(0, 180, 0); // y축 회전
+            StartCoroutine(FlipAnim());
+        }
+
+        private IEnumerator FlipAnim()
+        {
+            isFlipping = true;
+            float time = 0;
+
+            while (time < flipDuration)
+            {
+                time += Time.deltaTime;
+                float t = Mathf.Clamp01(time / flipDuration);
+                rt.localRotation = Quaternion.Slerp(startRot, endRot, t);
+                yield return null;
+            }
+            rt.localRotation = Quaternion.Euler(0, 0, 0);
+            cardImage.sprite = faceSprite;
+            endRot = startRot;
+            isFlipping = false;
+        }
     }
 }
