ff72d9574f7f1069b88f649a88f795e9b5b9686a
ParkMinSung2580 <114042812+ParkMinSung2580@users.noreply.github.com>
2025-08-21 11:12:49 +0900

    [Feat] 슈팅 미니게임 전용 GameState제작
    슈팅 미니게임 전용 GameState제작
    모든유저가 GameState를 가지고 게임 상태가 변해야 할 경우에는 마스터 클라이언트가 판단하고 ChangeState하는 형식으로 만들생각이다.

--- Assets/PMS/PMS_Scripts/ShootingGameManager.cs
+++ Assets/PMS/PMS_Scripts/ShootingGameManager.cs
@@ -1,8 +1,32 @@
 using UnityEngine;
+using Photon.Pun;
 using DesignPattern;
 
-public class ShootingGameManager : CombinedSingleton<ShootingGameManager>, IGameComponent , IGameStartHandler
+public class ShootingGameManager : PunSingleton<ShootingGameManager>, IGameComponent , IGameStartHandler
 {
+    private ShootingGameState currentState;
+
+    public int CurrentRound { get; private set; } = 0;
+    public int MaxRounds { get; private set; } = 3;
+
+    private void Start()
+    {
+        ChangeState(new InitState());
+    }
+
+    private void Update()
+    {
+        currentState?.Update();
+    }
+
+    //게임 상태 변경 (로컬 호출 금지!!!!!!!!!!!!!!!!!!!!!!, RPC 통해 호출)
+    public void ChangeState(ShootingGameState newState)
+    {
+        currentState?.Exit();
+        currentState = newState;
+        currentState.Enter();
+    }
+
     public void Initialize()
     {
         Debug.Log("[ShootingGameManager] - 슈팅 게임 초기화");
@@ -11,5 +35,15 @@ public class ShootingGameManager : CombinedSingleton<ShootingGameManager>, IGame
     public void OnGameStart()
     {
         Debug.Log("[ShootingGameManager] - 슈팅 게임 시작!");
+    }
+
+    [PunRPC]
+    public void RPC_ChangeState(string stateName)
+    {
+        switch (stateName)
+        {
+            case "InitState": ChangeState(new InitState()); break;
+            case "CardSelectState": ChangeState(new CardSelectState()); break;
+        }
     }
 }
