0d8be3fa882aa14c2cb6606ea73ec0177b4a9ed9
김승태 <tiga1207@naver.com>
2025-08-29 15:30:47 +0900

    [Chore] 판정바 스크립트 주석 추가
    판정바 스크립트가 붙을 게임 오브젝트는 Collider가 필요하기에 RequireComponent 애트리뷰트 추가
    추후 작업할 때 방향성을 잡아둘 주석 추가

--- Assets/KST/Scripts/RythmGame/Note/VerdictNote.cs
+++ Assets/KST/Scripts/RythmGame/Note/VerdictNote.cs
@@ -6,8 +6,12 @@ namespace RhythmGame
     /// 판정바 근처에 노트가 있는지 여부에 따라 
     /// 과열판정 등이 달라질 수 있도록 하는 클래스
     /// </summary>
+    [RequireComponent(typeof(Collider))]
     public class VerdictNote : MonoBehaviour
     {
+        //정확도 판정을 해당 판정바의 한 축을 기준으로 
+        //얼마나 중앙에 있는지 여부에 따라 accuracy가 달라지도록 할 필요가 잇음.
+        //
 
         /// <summary>
         /// 충돌체가 Note이면서 None 상태일 경우 Good 상태로 변경
