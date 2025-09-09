66bbefde7750a9567e2bbd52425913ab4333cea7
rnrgll <shuajjjjj@naver.com>
2025-09-09 15:40:24 +0900

    [Create] 설정창 UI 스크립트 생성

---
+++ Assets/LDH/LDH_Scripts/UI/01_Popup/UI_Popup_Setting.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace LDH_UI
+{
+    public class UI_Popup_Setting<Button> : UI_Popup
+    {
+        [SerializeField] private Button closeButton;
+     
+        
+        
+    }
+}
