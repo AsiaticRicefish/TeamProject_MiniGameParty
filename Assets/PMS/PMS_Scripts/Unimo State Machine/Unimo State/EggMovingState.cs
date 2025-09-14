f0e9149253104360b7d22da56a9a3d6b7f2362d6
ParkMinSung2580 <114042812+ParkMinSung2580@users.noreply.github.com>
2025-09-15 01:59:23 +0900

    [Refactor] 유니모 상태 패턴 추가
    상태 패턴 도입으로 로직 분리 - 유니모 상태에 각각의 역할만 작동하도록함.
    
    딕셔너리 기반 상태 매핑 - 외부에서 해당 유니모의 상태를 확인 할 수있거나 enum 기반 전환으로 편하게 전환 할 수 있도록 함
    
    RPC 기반 상태 승인 구조 - 클라이언트는 요청만 하고, 마스터 클라이언트가 검증 후 승인
    
    Tick / FixedTick 으로 상태별로 프레임 기반 로직과 물리 로직을 분리

---
+++ Assets/PMS/PMS_Scripts/Unimo State Machine/Unimo State/EggMovingState.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggMovingState : UnimoEggStateBase
+{
+    public override UnimoEggStateType StateType => UnimoEggStateType.Moving;
+    
+    public override void Enter(UnimoStateController egg)
+    {
+
+    }
+
+    public override void Tick(UnimoStateController egg)
+    {
+
+    }
+}
